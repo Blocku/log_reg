5e678c62418c9ab92bb11a516826e658c46ea408
Blocku b <urent111@yandex.ru>
2024-03-09 17:42:21 +0000

    3

--- form/src/lib/appwrite.js
+++ form/src/lib/appwrite.js
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('65e949710bde46e6cf6f'); // Replace with your project ID
+    .setProject('65e949710bde46e6cf6f');
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
