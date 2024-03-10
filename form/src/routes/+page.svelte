1ad3f6dba9ae5721e84179c34dd9573e0c3d1fab
Blocku b <urent111@yandex.ru>
2024-03-10 15:49:25 +0000

    12

--- form/src/routes/+page.svelte
+++ form/src/routes/+page.svelte
@@ -17,7 +17,7 @@
             Profile
         </button>
         <button class="btn btn_item-menu">
-            Settingss
+            Settings
         </button>
         <div class="w-full border rounded-full border-primary-200"></div>
         <button class="btn btn_item-menu">
