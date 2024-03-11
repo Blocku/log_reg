23cfc78e9e184b65ec37b3c17d22429f22199e13
Blocku b <urent111@yandex.ru>
2024-03-11 06:53:47 +0000

    5

--- form/src/routes/+page.svelte
+++ form/src/routes/+page.svelte
@@ -1,39 +1,55 @@
-<header class="w-full h-16 max-w-[88rem] mx-auto px-3 flex justify-between items-center border border-zinc-900">
-    <a href="/" class="logo">
-        Logo
-    </a>
-    <div class="md:hidden" >
-        <button on:click={handleClick} class="btn btn_menu">
-            <svg class="block h-5 w-5 text-primary-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="{icon}" />
-            </svg>
-        </button>
+<header class="w-full h-16 fixed top-0 right-0 left-0 border-b-2 border-primary-200 backdrop-blur-sm">
+    <div class="h-full max-w-[88rem] mx-auto flex justify-between items-center px-3">
+        <a href="/" class="logo">
+            <img class="h-10 w-10" src="../../Svelte_Logo.png" alt="logo">
+        </a>
+        <div class="md:hidden" >
+            <button on:click={handleClick} class="btn btn_menu">
+                <svg class="block h-5 w-5 text-primary-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="{icon}" />
+                </svg>
+            </button>
+        
+              
+        </div>
     
-          
+        <div bind:this={menu} class="hidden md:hidden p-1 space-y-1 absolute right-3 top-12 w-48 rounded-lg bg-white shadow-lg border-2 border-primary-200">
+            <button class="btn btn_item-menu">
+                Profile
+            </button>
+            <button class="btn btn_item-menu">
+                Settings
+            </button>
+            <div class="w-max border rounded-full border-primary-200"></div>
+            <button class="btn btn_item-menu">
+                Log out
+            </button>
+        </div>
+        <div class="relative hidden md:flex items-center ml-auto">
+            <nav class="text-md leading-6 font-medium text-slate-700" >
+                <ul class="flex space-x-8">
+                    <li class=" hover:text-orange-500 transition-all">
+                        <a href="/">Profile</a>
+                    </li>
+                    <li class=" hover:text-orange-500 transition-all">
+                        <a href="/">Settings</a>
+                    </li>
+                    <li class=" hover:text-orange-500 transition-all">
+                        <a href="/">Log out</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-
-    <div bind:this={menu} class="md:hidden p-1 space-y-1 absolute right-3 top-12 w-48 rounded-lg bg-white shadow-lg border-2 border-primary-200">
-        <button class="btn btn_item-menu">
-            Profile
-        </button>
-        <button class="btn btn_item-menu">
-            Settings
-        </button>
-        <div class="w-full border rounded-full border-primary-200"></div>
-        <button class="btn btn_item-menu">
-            Log out
-        </button>
-    </div>
-    <div class="relative hidden md:flex items-center ml-auto">
-        <nav class="text-md leading-6 font-medium text-slate-700" >
-            <ul class="flex space-x-8">
-                <li>Profile</li>
-                <li>Settings</li>
-                <li>Log out</li>
-            </ul>
-        </nav>
-    </div>
+    
 </header>
+
+<main class="w-wull h-[300dvh] mt-28 flex gap-10 border border-zinc-500">
+    <section class=" break-all shadow-lg">
+dfdsafdsafsadfsadfasdfadsfdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </section>
+
+</main>
 
 
 
